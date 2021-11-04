608c7d86daece9f220704988dcf01c3b7bc954a6
Gocek <gocek89@gmail.com>
2021-11-04 20:45:02 +0100

    add loop for and print coment

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -2,3 +2,7 @@ shoping_list = {
     "piekarnia": ["chleb", "bułka tarta", "bułki"],
     "warzywniak": ["pomidory", "ogórek", "ziemniaki"],
     "zabka": ["mleko", "masło orzechowe", "chipsy"]}
+
+for kay, value in shoping_list.items():
+    print(
+        f"Idę do {kay.capitalize()} ,kupuję tu następujące rzeczy: {value}")

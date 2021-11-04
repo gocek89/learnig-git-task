41c30b3ef16623786fca68815a3e13ef3c93b7b2
Gocek <gocek89@gmail.com>
2021-11-04 20:51:21 +0100

    Add counter to program

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -1,10 +1,15 @@
 shoping_list = {
     "piekarnia": ["chleb", "bułka tarta", "bułki"],
     "warzywniak": ["pomidory", "ogórek", "ziemniaki"],
-    "zabka": ["mleko", "masło orzechowe", "chipsy"]}
-
+    "zabka": ["mleko", "masło orzechowe", "chipsy", "test"]}
+sum = 0
 for kay, value in shoping_list.items():
     for v in range(len(value)):
         value[v] = value[v].capitalize()
     print(
         f"Idę do {kay.capitalize()} ,kupuję tu następujące rzeczy: {value}")
+
+    x = len(value)
+    sum += x
+
+print(f"Suma wszystkich produktów wynosi {sum}")

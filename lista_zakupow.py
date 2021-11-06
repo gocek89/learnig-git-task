5848c24fd2b9ba1552babd14b489b0855c4665c1
Gocek <gocek89@gmail.com>
2021-11-06 16:33:10 +0100

    changing the program to regards

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -1,15 +1,2 @@
-shoping_list = {
-    "piekarnia": ["chleb", "bułka tarta", "bułki", "kokosanki"],
-    "warzywniak": ["pomidory", "ogórek", "ziemniaki"],
-    "zabka": ["mleko", "masło orzechowe", "chipsy", "papierosy"]}
-sum = 0
-for kay, value in shoping_list.items():
-    for v in range(len(value)):
-        value[v] = value[v].capitalize()
-    print(
-        f"Idę do {kay.capitalize()} ,kupuję tu następujące rzeczy: {value}")
 
-    x = len(value)
-    sum += x
-
-print(f"Suma wszystkich produktów wynosi {sum}")
+print("Dzień Dobry Panie Pawle, miło mi poinformować ze obsluga gita przyhodzi mi coraz łatwiej")
